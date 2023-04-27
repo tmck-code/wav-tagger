5496eb6ef94f193a331b24207c8cacdd1215e36e
Tom McKeesick <tmck01@gmail.com>
2023-04-27 22:56:44 +1000

    update poc
    add a metadata JSON file instead of trying to parse from the filename

--- poc.py
+++ poc.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python3
+'''
+usage: python3 poc.py <dirpath> <metadata.json>
+
+metadata format:
+{
+    "album": "album name",
+    "tracks": [
+        {"track": "01", "title": "track 1", "artist": "artist 1", "genre": "genre 1"},
+        {"track": "02", "title": "track 2", "artist": "artist 2", "genre": "genre 2"},
+    ]
+}
+'''
+
 
 from wav_tagger import tag
 import os, json, glob, sys
 
-dirpath = sys.argv[1]
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import TerminalTrueColorFormatter
 
-for fpath in glob.glob(f'{dirpath}/*.wav'):
-    artist, album, remainder = os.path.splitext(os.path.basename(fpath))[0].split(' - ', 2)
-    print(f'{artist=}, {album=}, {remainder=}')
-    track_number, title = remainder.strip().split(' ', 1)
-    print(f'{track_number=}, {title=}')
-    metadata = {
-        'artist': artist,
-        'title': title,
-        'album': album,
-        'track': str(int(track_number)),
-        'ITRK': str(int(track_number)),
-    }
-    print(json.dumps(metadata, indent=2))
-    input('correct?')
-    tag.write_wav_metadata(fpath=fpath, metadata=metadata)
+def ppd(d, indent=2):
+    'pretty-prints a dict'
+    print(highlight(
+        code      = json.dumps(d, indent=indent),
+        lexer     = JsonLexer(),
+        formatter = TerminalTrueColorFormatter()
+    ).strip())
+
+def run(dirpath: str, metadata_fpath: str):
+    metadata = json.load(open(metadata_fpath))
+
+    print('loaded metadata')
+    ppd(metadata)
+    for i, (fpath, track) in enumerate(zip(sorted(glob.glob(f'{dirpath}/*.wav')), metadata['tracks'])):
+        metadata = {
+            'artist': track['artist'],
+            'title': track['title'],
+            'album': metadata['album'],
+            'genre': track['genre'],
+            'track': str(int(track['track'])),
+            'ITRK': str(int(track['track'])),
+        }
+        ppd(metadata)
+        print(fpath)
+        input('correct?')
+        tag.write_wav_metadata(fpath=fpath, metadata=metadata)
+
+if __name__ == '__main__':
+    run(dirpath=sys.argv[1], metadata_fpath=sys.argv[2])
+
