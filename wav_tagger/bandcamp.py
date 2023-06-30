fc0765e4d76cc79d84a99d785d7d5a71eb7cd425
Tom McKeesick <tmck01@gmail.com>
2023-07-01 00:08:17 +1000

    update bandcamp

--- wav_tagger/bandcamp.py
+++ wav_tagger/bandcamp.py
@@ -1,19 +1,29 @@
 import json
 import requests
 
-def parse_tracks(data, album_artist):
+def parse_tracks(data):
     for track in data['track']['itemListElement']:
-        yield {'track': track['position'], 'title': track['item']['name'], 'artist': album_artist}
+        yield {
+            'track': track['position'],
+            'title': track['item']['name'],
+            'artist': data['albumArtist'],
+        }
 
+def fetch_metadata(url):
+    response = requests.get(url)
 
-response = requests.get('https://divididmusic.bandcamp.com/album/year-of-i')
-
-for i, line in enumerate(response.text.split('\n')):
-    if '/track/' in line.lower():
+    for i, line in enumerate(response.text.split('\n')):
+        if '/track/' not in line.lower():
+            continue
         try:
-            data = json.loads(line.strip())
-            for track in parse_tracks(data, 'Tom Finster'):
-                print(track)
+            return json.loads(line.strip())
         except ValueError as e:
-            print(':(', e, i)
+            pass
 
+def fetch_tracks(url):
+    data = fetch_metadata(url)
+    yield from parse_tracks(data)
+
+if __name__ == '__main__':
+    for t in fetch_tracks('https://polabryson.bandcamp.com/album/beneath-the-surface'):
+        print(t)
