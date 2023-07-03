7defe59ddc8245cd5aa69973040a197f3957cf4b
Tom McKeesick <tmck01@gmail.com>
2023-07-03 16:34:02 +1000

    update bandcamp bin

--- wav_tagger/bandcamp.py
+++ wav_tagger/bandcamp.py
@@ -33,6 +33,34 @@ def parse_metadata_from_fname(fname) -> (str, dict):
     artist, album, track, title = m.groups()
     return album, {'artist': artist, 'track': track, 'title': title}
 
+def unzip(fpath):
+    import zipfile
+    with zipfile.ZipFile(fpath, 'r') as zip_ref:
+        zip_ref.extractall(os.path.dirname(fpath))
+
+
+def parse_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-fpath', type=str, required=True)
+    argparser.add_argument('-genre', type=str)
+    args = argparser.parse_args().__dict__
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # 1. unzip the file
+    # 2. create metadata config by parsing filenames
+    # 3. apply the metadata
+
+    metadata = create_bandcamp_metadata(dirpath=args['dirpath'], genre=args['genre'], ofpath='metadata.json')
+    ppd(asdict(metadata))
+
+    choice = input('write metadata?')
+    if choice.lower() in ('y', 'yes'):
+        run_with_metadata(dirpath=args['dirpath'], metadata_fpath='metadata.json')
+
+
 
 if __name__ == '__main__':
     for t in fetch_tracks(url=sys.argv[1]):
